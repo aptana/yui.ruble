ee3a556826b4b4d96d61f6e9b2c681ef4d6ab6b5
Ingo Muschenetz <ingo@appcelerator.com>
2011-04-15 21:58:24 -0700

    Added 2.8 and 3.0 SDOCML files.

---
+++ bundle.rb
@@ -0,0 +1,8 @@
+require 'ruble'
+
+bundle 'YUI' do |bundle|
+  bundle.author = 'Ingo Muschenetz'
+  bundle.description =  <<END
+Support for the <a href="http://developer.yahoo.com/yui/">YUI JavaScript library</a>.
+END
+end
